9e3bbdcee09c33688f951c26a57aff3dba1acf5b
Luis Cordova <lcordovacarrion@gmail.com>
2024-05-01 16:32:13 -0500

    Clase

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author utpl
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        // En esta clase principal en el metodo main se crea un objeto, que 
+        // utiliza un conector predterminado que da valores por defecto a los
+        // atributos, ya que no existe ningun conector especifico para el
+        // objeto.
+        Computadora computadoraTrabajo = new Computadora();
+        
+    }
+}
